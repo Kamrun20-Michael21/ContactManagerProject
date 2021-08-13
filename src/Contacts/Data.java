81923399473cf09b1f6e25a5ed062f4585100d86
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-13 15:22:12 -0500

    Successful test of writing to file

--- src/Contacts/Data.java
+++ src/Contacts/Data.java
@@ -1,4 +1,33 @@
 package Contacts;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Data {
+    public static void main(String[] args) throws IOException {
+
+        String directory = "data";
+        String filename = "contacts.txt";
+
+        Path dataDirectory = Paths.get(directory);
+        Path dataFile = Paths.get(directory, filename);
+
+        if (Files.notExists(dataDirectory)) {
+            Files.createDirectories(dataDirectory);
+        }
+
+        if (! Files.exists(dataFile)) {
+            Files.createFile(dataFile);
+        }
+
+        List<String> lines = Arrays.asList("testing: 1, 2, 3", "testing: 2, 3, 4"); // create new List of strings
+
+        Files.write(dataFile, lines);
+    }
 }
