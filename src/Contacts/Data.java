45e5b6695935aa5471bd1ce37b4e30114bd2eece
Kamrun Foster <kamrun.foster4@gmail.com>
2021-08-13 16:12:58 -0500

    added fori loop alternative, updating contact file

--- src/Contacts/Data.java
+++ src/Contacts/Data.java
@@ -16,25 +16,31 @@ public class Data {
         String filename = "contacts.txt";
 
         Path dataDirectory = Paths.get(directory);
-        Path dataFile = Paths.get(directory, filename);
+        Path contactFile = Paths.get(directory, filename);
 
         if (Files.notExists(dataDirectory)) {
             Files.createDirectories(dataDirectory);
         }
 
-        if (! Files.exists(dataFile)) {
-            Files.createFile(dataFile);
+        if (! Files.exists(contactFile)) {
+            Files.createFile(contactFile);
         }
 
 
-        List<String> contentReadsFromFile = Files.readAllLines(dataFile);// will read a files content
+        List<String> contentReadsFromFile = Files.readAllLines(contactFile);// will read a files content
         List<String> lines = Arrays.asList("testing: 1, 5, 3", "testing: 2, 7, 4"); // create new List of strings
-        Files.write(dataFile, lines);
+        Files.write(contactFile, lines);
 
         for (String line: contentReadsFromFile){
             System.out.println(line);
         }
+        //alternative to our Enhanced for loop
+//        for (int i = 0; i < lines.size(); i++) {
+//            System.out.println((i+1)+": "+lines.get(i));
+//        }
 
 
+        // *********************END OF MAIN METHOD*********************
     }
+    // *********************END OF DATA Class*********************
 }
