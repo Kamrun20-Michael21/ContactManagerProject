4bd7103505a4047649de6590236cba0041510dec
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-16 09:47:49 -0500

    Created variables and constructor for Contact class

--- src/Contacts/Contact.java
+++ src/Contacts/Contact.java
@@ -1,7 +1,15 @@
 package Contacts;
 
 public class Contact {
+    private String firstName;
+    private String lastName;
+    private int phoneNumber;
 
+    public Contact(String firstName, String lastName, int phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
 
     public static void main(String[] args) {
         // TODO: 8/13/21 create list of strings, iterate through string to
